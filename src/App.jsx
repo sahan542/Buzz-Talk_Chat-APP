358e608d3913f30b95612f0fb0174327c53201ee
sahan542 <sahanrashmika542@gmail.com>
2024-02-04 22:08:19 +0530

    Connected to momgo DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Register from './Register'
+import axios from 'axios'
 
 
 function App() {
+
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
 
   return (
     <>
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 export default function Register(){
@@ -5,11 +6,14 @@ export default function Register(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-
+    async function register(ev){
+        ev.preventDefault();
+        await axios.post('/register', {username,password});
+    }
 
     return (
         <div className="bg-blue-50 h-screen flex items-center">
-            <form className="w-64 mx-auto mb-12" >
+            <form className="w-64 mx-auto mb-12" onSubmit={register}>
                 <input 
                     type ="text"
                     placeholder="username"
