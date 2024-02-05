947181101ce6e2568dc3c7195caff99e16365929
sahan542 <sahanrashmika542@gmail.com>
2024-02-06 02:59:13 +0530

    connecting web sockets

---
+++ src/Chat.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+
+export default function Chat() {
+    useEffect(() => {
+        new WebSocket('ws://localhost:4040');
+    }, []);
+
+    return (
+        <div className="flex h-screen">
+            <div className="bg-white w-1/3">Contacts</div>
+            <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+                <div className="flex-grow">Messages with selected person</div>
+                <div className="flex gap-2 mx-2">
+                    <input 
+                        type="text" 
+                        placeholder="Type your message here" 
+                        className="bg-white flex-grow border rounded-sm p-2" 
+                    />
+                    <button className="bg-blue-500 p-2 text-white rounded-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
 import RegisterAndLoginForm from "./RegisterAndLoginForm.jsx";
+import Chat from "./Chat.jsx";
 
 
 export default function Routes(){
     const {username, id} = useContext(UserContext);
 
     if(username){
-        return 'Logged In ' + username;
+        return <Chat />;
     }
     return(
         <RegisterAndLoginForm/>
