5a8abbb86a0f6dc8b041346fa9b06a80d6fa36dd
sahan542 <sahanrashmika542@gmail.com>
2024-02-10 02:03:40 +0530

    share files & attachments

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,5 +1,4 @@
 import { useContext, useEffect, useRef, useState } from "react"
-import Avatar from "./Avatar";
 import Logo from "./Logo";
 import { UserContext } from "./UserContext";
 import uniqBy from 'lodash/uniqBy';
@@ -52,11 +51,13 @@ export default function Chat() {
         }
       }
 
-      function sendMessage(ev) {
+      function sendMessage(ev, file = null) {
+        if(ev)
         ev.preventDefault();
         ws.send(JSON.stringify({
             recipient: selecteduserId,
             text: newMessageText,
+            file,
         }));
         setNewMessageText('');
         setMessages(prev => ([...prev,{
@@ -65,6 +66,12 @@ export default function Chat() {
             recipient: selecteduserId,
             _id: Date.now(),
             }]));
+        if (file) {
+            axios.get('/messages/'+selecteduserId).then(res => {
+                setMessages(res.data);
+            });
+        }
+        
       }
 
       function logout(){
@@ -75,6 +82,17 @@ export default function Chat() {
         });
       }
 
+      function sendFile(ev) {
+        const reader = new FileReader();
+        reader.readAsDetaURL(ev.target.files[0]);
+        reader.onload = () => {
+            sendMessage(null, {
+                name: ev.target.files[0].name,
+                data: reader.result,
+
+            });
+        };
+      }
       useEffect(() => {
         const div = divUnderMessages.current;
         if (div){
@@ -169,6 +187,16 @@ export default function Chat() {
                                 sender: {message.sender}<br/>
                                 my id: {id}<br/>
                                 {message.text}
+                                {message.file && (
+                                    <div className="flex items-center gap-1">
+                                        <a className="flex items-center gap-1 border-b" target="_blank" href={axios.defaults.baseURL + '/' + message.file} >
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                        <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 0 0-3.182 0l-10.94 10.94a3.75 3.75 0 1 0 5.304 5.303l7.693-7.693a.75.75 0 0 1 1.06 1.06l-7.693 7.693a5.25 5.25 0 1 1-7.424-7.424l10.939-10.94a3.75 3.75 0 1 1 5.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 0 1 5.91 15.66l7.81-7.81a.75.75 0 0 1 1.061 1.06l-7.81 7.81a.75.75 0 0 0 1.054 1.068L18.97 6.84a2.25 2.25 0 0 0 0-3.182Z" clipRule="evenodd" />
+                                        </svg>
+                                        {message.file}
+                                        </a>
+                                    </div>
+                                )}
                             </div>
                         </div>
                     ))}
@@ -191,6 +219,13 @@ export default function Chat() {
                   placeholder="Type your message here"
                   className="bg-white flex-grow border rounded-sm p-2"
                 />
+                <label type="button" className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-300">
+                    <input type="file" className="hidden" onChange={sendFile} />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 0 0-3.182 0l-10.94 10.94a3.75 3.75 0 1 0 5.304 5.303l7.693-7.693a.75.75 0 0 1 1.06 1.06l-7.693 7.693a5.25 5.25 0 1 1-7.424-7.424l10.939-10.94a3.75 3.75 0 1 1 5.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 0 1 5.91 15.66l7.81-7.81a.75.75 0 0 1 1.061 1.06l-7.81 7.81a.75.75 0 0 0 1.054 1.068L18.97 6.84a2.25 2.25 0 0 0 0-3.182Z" clipRule="evenodd" />
+                </svg>
+
+                </label>
                 <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
